e1ac7f1cf4890c93d2e1faa40df9a77060fb07ec
Martin Pitt <martin.r.pitt@gmail.com>
2025-06-01 15:24:46 +0100

    Rename

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,8 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: 'SLua Dev',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/Martin-Pitt/slua.dev' }],
 			sidebar: [
 				{
 					label: 'Guides',
