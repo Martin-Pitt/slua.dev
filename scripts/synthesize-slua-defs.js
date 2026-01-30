cd8894e2b5ffb20cf880998a6838e48a8812b1cc
Tapple Gao <tapplek@gmail.com>
2026-01-29 19:48:31 -0800

    Add ReplaceSubString mapping to synthesize-slua-defs.js

--- scripts/synthesize-slua-defs.js
+++ scripts/synthesize-slua-defs.js
@@ -53,6 +53,7 @@ const DuplicateFunctions = {
 	'Char': 'string.char, utf8.char',
 	'Ord': 'string.byte, utf8.codepoint',
 	'SubStringIndex': 'string.find',
+	'ReplaceSubString': 'string.gsub',
 	'ParseString2List': 'string.split',
 	'ParseStringKeepNulls': 'string.split',
 	'CSV2List': 'string.split',
